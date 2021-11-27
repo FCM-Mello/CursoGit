1718b5639b48f59e4681a5405fdd48b8974fc6d8
Filipe de Mello Cunha <filipemello.cunha@gmail.com>
2021-11-26 21:34:18 -0300

    Mais M oi

--- TesteDoGit/Program.cs
+++ TesteDoGit/Program.cs
@@ -7,7 +7,7 @@ namespace TesteDoGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
+            Console.WriteLine("oi");
 
             Console.ReadLine();
         }
